31c3776f822471dad11ac0615cdf4b6b5cd59917
arsyahendratno <103812197+arsyahendratno@users.noreply.github.com>
2023-10-08 21:43:48 +0700

    Update DAY_13.sql

--- DAY_13.sql
+++ DAY_13.sql
@@ -61,45 +61,54 @@ SELECT
     MAX(u.register_date) AS last_login,
     -- The last_activity is derived from the maximum timestamp from different event sources, namely registration, transactions, ad clicks, and user events.
     MAX(COALESCE(u.register_date, ut.transaction_date, fa.timestamp, ia.timestamp, ue.timestamp)) AS last_activity,
-    COALESCE(ut.total_transactions, 0) AS total_transactions,
-    COALESCE(fa.total_clicks, 0) + COALESCE(ia.total_clicks, 0) AS total_ads_clicks,
-    COALESCE(ue.total_events, 0) AS total_events,
+    COALESCE(SUM(ut.total_transactions), 0) AS total_transactions,
+    COALESCE(SUM(fa.total_clicks), 0) + COALESCE(SUM(ia.total_clicks), 0) AS total_ads_clicks,
     -- The engagement score is determined by calculating the mean of the user's activities.
-    -- total_transactions + total_ads_clicks + total_events) / 3
-    ((COALESCE(ut.total_transactions, 0) + COALESCE(fa.total_clicks, 0) + COALESCE(ia.total_clicks, 0) + COALESCE(ue.total_events, 0)) / 3.0) AS engagement_score
-FROM users u
-JOIN (
-    SELECT user_id, COUNT(*) AS total_transactions
-    FROM user_transactions
-    GROUP BY user_id
+    -- total_transactions + total_ads_clicks + total_events / 3
+    COALESCE(SUM(ue.total_events), 0) AS total_events,
+    (
+        (COALESCE(SUM(ut.total_transactions), 0) + COALESCE(SUM(fa.total_clicks), 0) + COALESCE(SUM(ia.total_clicks), 0) + COALESCE(SUM(ue.total_events), 0)) / 3.0
+    ) AS engagement_score
+FROM "user".users u
+LEFT JOIN (
+    SELECT user_id, transaction_date, COUNT(*) AS total_transactions 
+    FROM "user".user_transactions ut
+    GROUP BY user_id, transaction_date
 ) ut ON u.id = ut.user_id
-JOIN (
-    SELECT user_id, COUNT(*) AS total_clicks
-    FROM facebook_ads
-    GROUP BY user_id
-    UNION ALL
-    SELECT user_id, COUNT(*) AS total_clicks
-    FROM instagram_ads
-    GROUP BY user_id
-) AS fa ON u.id = fa.user_id
-JOIN (
-    SELECT user_id, COUNT(*) AS total_events
-    FROM user_event
-    GROUP BY user_id
-) AS ue ON u.id = ue.user_id
-JOIN (
-    SELECT user_id, MAX(timestamp) AS timestamp
+LEFT JOIN (
+    SELECT user_id, timestamp, COUNT(*) AS total_clicks
     FROM (
-        SELECT user_id, timestamp FROM facebook_ads
+        SELECT user_id, "timestamp" FROM social_media.facebook_ads fa
+        JOIN "user".users u ON fa.id = u.client_id
+        JOIN "user".user_transactions ut ON u.id = ut.user_id
         UNION ALL
-        SELECT user_id, timestamp FROM instagram_ads
+        SELECT user_id, "timestamp" FROM social_media.instagram_ads ia
+        JOIN "user".users u ON ia.id = u.client_id
+        JOIN "user".user_transactions ut ON u.id = ut.user_id
+    ) AS combined_ads
+    GROUP BY user_id, timestamp
+) AS fa ON u.client_id = fa.user_id
+LEFT JOIN (
+    SELECT user_id, timestamp, COUNT(*) AS total_events
+    FROM "event"."User Event" ue
+    GROUP BY user_id, timestamp
+) AS ue ON u.id = ue.user_id
+LEFT JOIN (
+    SELECT user_id, MAX("timestamp") AS "timestamp", COUNT(*) AS total_clicks
+    FROM (
+        SELECT user_id, "timestamp" FROM social_media.facebook_ads fa
+        JOIN "user".users u ON fa.id = u.client_id
+        JOIN "user".user_transactions ut ON u.id = ut.user_id
         UNION ALL
-        SELECT user_id, timestamp FROM user_event
+        SELECT user_id, "timestamp" FROM social_media.instagram_ads ia
+        JOIN "user".users u ON ia.id = u.client_id
+        JOIN "user".user_transactions ut ON u.id = ut.user_id
+        UNION ALL
+        SELECT user_id, "timestamp" FROM "event"."User Event" ue
     ) AS combined_events
     GROUP BY user_id
-) AS ia ON u.id = ia.user_id
+) AS ia ON u.client_id = ia.user_id
 GROUP BY u.id;
-
 
 -- 2b.                                                                                                                                                CREATE TABLE fakta_iklan_kinerja AS
 SELECT
